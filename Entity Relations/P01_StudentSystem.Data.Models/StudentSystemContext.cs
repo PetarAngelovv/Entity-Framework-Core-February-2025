da5ab826dfb6dc7eb0cfe1309199baf04ea0a25d
Petar_Angelov <101021042+PetarAngelovv@users.noreply.github.com>
2025-03-03 19:34:22 +0200

    Add files via upload

---
+++ Entity Relations/P01_StudentSystem.Data.Models/StudentSystemContext.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class StudentSystemContext : DbContext
+    {
+        public StudentSystemContext()
+        {
+
+        }
+
+        public StudentSystemContext(DbContextOptions<StudentSystemContext> options)
+           : base(options)
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Homework> Homeworks { get; set; }
+        public DbSet<Resource> Resources { get; set; }
+        public DbSet<StudentCourse> StudentsCourses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            if (!optionsBuilder.IsConfigured)
+            {
+              optionsBuilder.UseSqlServer("Server=.;Database=Student_System;Trusted_Connection=True;");
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudentCourse>()
+                .HasKey(sc => new { sc.CourseId, sc.StudentId });
+
+            modelBuilder.Entity<StudentCourse>()
+                .HasOne(sc => sc.Student)
+                .WithMany(sc => sc.StudentsCourses)
+                .HasForeignKey(sc => sc.StudentId);
+
+            modelBuilder.Entity<StudentCourse>()
+                .HasOne(sc => sc.Course)
+                .WithMany(sc => sc.StudentsCourses)
+                .HasForeignKey(sc => sc.CourseId);
+
+            modelBuilder.Entity<Resource>()
+                .Property(r => r.Url)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Homework>()
+                .Property(h => h.Content)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Student>()
+              .Property(s => s.PhoneNumber)
+              .IsUnicode(false);
+        }
+       
+    }
+}
---
+++ Entity Relations/P01_StudentSystem/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_StudentSystem.Data.Models;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Exercises_Entity_Relations
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var optionsBuilder = new DbContextOptionsBuilder<StudentSystemContext>();
+            optionsBuilder.UseSqlServer("Server=.;Database=Student_System;Trusted_Connection=True;");
+
+            using (var context = new StudentSystemContext(optionsBuilder.Options))
+            {
+
+                context.Database.Migrate(); // Това ще създаде базата данни и таблиците, ако още не съществуват
+            }
+
+            Console.WriteLine("Db is created!");
+        }
+    }
+}
