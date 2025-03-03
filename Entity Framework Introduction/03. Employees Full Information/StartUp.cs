e752a9f7005c64c3ffbd4051f9a2fd3a2b4c2579
Petar_Angelov <101021042+PetarAngelovv@users.noreply.github.com>
2025-03-03 23:33:28 +0200

    Add files via upload

---
+++ Entity Framework Introduction/03. Employees Full Information/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using SoftUni.Data;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext DbContext = new SoftUniContext();
+            DbContext.Database.EnsureCreated();
+
+            string result = GetEmployeesFullInformation(DbContext);
+            Console.WriteLine(result);
+        }
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+            var employees = context.Employees
+                .Select(e => new { 
+                 e.FirstName,
+                 e.LastName,
+                 e.MiddleName,
+                 e.JobTitle,
+                e.Salary
+                })
+                .ToArray();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary.ToString("F2")}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
