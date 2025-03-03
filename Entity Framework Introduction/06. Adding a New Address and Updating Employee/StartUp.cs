f26bab3a93edaf56e60281961f61a2bdf2b0f0f0
Petar_Angelov <101021042+PetarAngelovv@users.noreply.github.com>
2025-03-03 23:37:07 +0200

    Add files via upload

---
+++ Entity Framework Introduction/06. Adding a New Address and Updating Employee/StartUp.cs
@@ -0,0 +1,124 @@
+﻿using SoftUni.Data;
+using SoftUni.Models;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+           SoftUniContext DbContext = new SoftUniContext();
+           DbContext.Database.EnsureCreated();
+
+        // string result = GetEmployeesFullInformation(DbContext);
+        // Console.WriteLine(result);
+
+        // string result = GetEmployeesWithSalaryOver50000(DbContext);
+        // Console.WriteLine(result);
+
+        // string result = GetEmployeesFromResearchAndDevelopment(DbContext);
+        // Console.WriteLine(result);
+        
+           string result = AddNewAddressToEmployee(DbContext);
+           Console.WriteLine(result);
+        }
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+            var employees = context.Employees
+                .Select(e => new { 
+                 e.FirstName,
+                 e.LastName,
+                 e.MiddleName,
+                 e.JobTitle,
+                e.Salary
+                })
+                .ToArray();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary.ToString("F2")}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context) 
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context.Employees
+                .Select(e => new {
+                    e.FirstName,
+                    e.Salary
+                })
+                .Where(e => e.Salary > 50_000)
+                .OrderBy(e => e.FirstName)
+                .ToArray();
+            foreach(var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} - {employee.Salary.ToString("F2")}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+            var employees = context.Employees
+                .Where(e => e.Department.Name == "Research and Development")
+                .Select(e => new{
+                    e.FirstName,
+                    e.LastName,
+                    DepartmentName = e.Department.Name,
+                    e.Salary
+                })
+                .OrderBy(e => e.Salary)
+                .ThenByDescending(e => e.FirstName)
+                .ToArray();
+
+            foreach (var e in employees)
+            {
+                sb.AppendLine($"{e.FirstName} {e.LastName} from {e.DepartmentName} - ${e.Salary:F2}");
+            }
+                return sb.ToString().TrimEnd();
+        }
+        public static string AddNewAddressToEmployee(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+            const string newAdressText = "Vitoshka 15";
+            const int newTownId = 4;
+
+            Address newAdress = new Address()
+            {
+                AddressText = newAdressText,
+                TownId = newTownId
+            };
+
+            var Nakovemployees = context.Employees
+                            .First(e => e.LastName.Equals("Nakov"));
+
+            Nakovemployees.Address = newAdress;
+
+            context.SaveChanges();
+
+
+
+            string?[] Addresses = context
+                .Employees
+                .Where(e=> e.AddressId.HasValue)
+                .OrderByDescending(e => e.AddressId)
+                .Select(e => e.Address!.AddressText)
+                .Take(10)
+                .ToArray();
+            
+            return String.Join(Environment.NewLine, Addresses);
+
+        }
+
+
+    }
+
+
+}
