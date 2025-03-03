c19ad66a11c2ea5b3cdfbe151175d37247749ef9
Petar_Angelov <101021042+PetarAngelovv@users.noreply.github.com>
2025-03-03 19:47:01 +0200

    Add files via upload

---
+++ Entity Relations/P01_StudentSystem/P01_StudentSystem.Data.Models/Migrations/20250222113911_FixedLastNames.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace P01_StudentSystem.Data.Models.Migrations
+{
+    public partial class FixedLastNames : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Entity Relations/P01_StudentSystem/P01_StudentSystem.Data.Models/Student.cs
@@ -0,0 +1,37 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class Student
+    {
+
+        public Student()
+        {
+            StudentsCourses = new HashSet<StudentCourse>();
+            Homeworks = new HashSet<Homework>();
+        }
+        public int StudentId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Unicode(false)]
+        [StringLength(10, MinimumLength = 10)]
+        public string? PhoneNumber { get; set; }
+
+        [Required]
+        public DateTime RegisteredOn { get; set; }
+
+        public DateTime? Birthday { get; set; }
+
+        public ICollection<StudentCourse> StudentsCourses { get; set; }
+        public ICollection<Homework> Homeworks { get; set; }
+
+
+
+    }
+}
