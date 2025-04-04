ef23ed290ebaa2a14989299ecba409f324874e4a
Petar_Angelov <101021042+PetarAngelovv@users.noreply.github.com>
2025-04-04 18:32:41 +0300

    Add files via upload

---
+++ JavaScript Object Notation - JSON/07. Export Categories By Products Count/DTOs/Import/ImportCategoryDto.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace ProductShop.DTOs.Import
+{
+    using Newtonsoft.Json;
+    using System.ComponentModel.DataAnnotations;
+
+    public class ImportCategoryDto
+    {
+        [Required]
+        [JsonProperty("name")]
+        public string? Name { get; set; }
+    }
+}
