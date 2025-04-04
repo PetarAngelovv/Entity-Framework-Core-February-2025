c8067fa95bdbfe81649786fc7e0dce8a3e9aec36
Petar_Angelov <101021042+PetarAngelovv@users.noreply.github.com>
2025-04-04 18:33:01 +0300

    Add files via upload

---
+++ JavaScript Object Notation - JSON/09. Import Suppliers/Models/PartCar.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarDealer.Models
+{
+    public class PartCar
+    {
+        [Required]
+        [ForeignKey(nameof(Part))]
+        public int PartId { get; set; }
+        public virtual Part Part { get; set; } = null!;
+
+        [Required]
+        [ForeignKey(nameof(Car))]
+        public int CarId { get; set; }
+        public virtual Car Car { get; set; } = null!; 
+    }
+}
---
+++ JavaScript Object Notation - JSON/09. Import Suppliers/StartUp.cs
@@ -0,0 +1,69 @@
+﻿using CarDealer.Data;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext dbContext = new CarDealerContext();
+
+            string jsonString = File.ReadAllText("../../../Datasets/suppliers.json");
+            Console.WriteLine(ImportSuppliers(dbContext, jsonString));
+        }
+        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+        {
+            string result = string.Empty;
+
+            ImportSupplierDto[]? supplierDtos = JsonConvert.DeserializeObject<ImportSupplierDto[]>(inputJson);
+
+            if (supplierDtos != null)
+            {
+                ICollection<Supplier> suppliersToAdd = new HashSet<Supplier>();
+                foreach(var dto in supplierDtos)
+                {
+                    if (!IsValid(dto))
+                    {
+                        continue;
+                    }
+                    if (dto.Name == null)
+                    {
+                        continue;
+                    }
+                    bool isValidImporter = bool.TryParse(dto.isImporter, out bool ParsedImporter);
+
+                    if (!isValidImporter)
+                    {
+                        continue;
+                    }
+
+                    Supplier supplier = new Supplier() {
+                        
+                        Name = dto.Name,
+                        IsImporter = ParsedImporter
+                    
+                    };
+                    suppliersToAdd.Add(supplier);
+                }
+                context.Suppliers.AddRange(suppliersToAdd);
+                context.SaveChanges();
+                result = $"Successfully imported {suppliersToAdd.Count}.";
+            }
+            return result;
+        }
+
+        public static bool IsValid(object dto)
+        {
+            var validateContext = new ValidationContext(dto);
+            var validateResults = new List<ValidationResult>();
+
+            bool isValid = Validator.TryValidateObject(dto, validateContext, validateResults, true);
+
+            return isValid;
+        }
+    }
+}
