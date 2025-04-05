66a662ac17b66e947fe5f7df76adbc1394ae2d56
Petar_Angelov <101021042+PetarAngelovv@users.noreply.github.com>
2025-04-05 22:47:16 +0300

    Add files via upload

---
+++ JavaScript Object Notation - JSON/11. Import Cars/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarDealer.Models
+{
+    public class Customer
+    {
+        [Required]
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public virtual DateTime BirthDate { get; set; }
+
+        public bool IsYoungDriver { get; set; }
+
+        public virtual ICollection<Sale> Sales { get; set; } = new List<Sale>(); 
+    }
+}
