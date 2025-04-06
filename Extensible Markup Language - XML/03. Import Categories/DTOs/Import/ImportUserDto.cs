2b1d83299d4cae0579503c30160f2f6b31bc6fd2
Petar_Angelov <101021042+PetarAngelovv@users.noreply.github.com>
2025-04-06 14:35:18 +0300

    Add files via upload

---
+++ Extensible Markup Language - XML/03. Import Categories/DTOs/Import/ImportUserDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ProductShop.DTOs.Import
+{
+    [XmlType("User")]
+    public class ImportUserDto
+    {
+        [XmlElement("firstName")]
+        public string FirstName { get; set; }
+
+        [XmlElement("lastName")]
+        public string LastName { get; set; }
+
+        [XmlElement("age")]
+        public string Age { get; set; }
+
+    }
+}
