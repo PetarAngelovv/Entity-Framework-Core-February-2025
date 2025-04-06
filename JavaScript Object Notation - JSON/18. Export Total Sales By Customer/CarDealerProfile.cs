458d379ce0d8f75c7cc2f2ccaf86130771a6140f
Petar_Angelov <101021042+PetarAngelovv@users.noreply.github.com>
2025-04-06 14:13:28 +0300

    Add files via upload

---
+++ JavaScript Object Notation - JSON/18. Export Total Sales By Customer/CarDealerProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+
+namespace CarDealer
+{
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            CreateMap<ImportCarDto, Car>()
+            .ForMember(dest => dest.PartsCars, opt => opt.Ignore()); // Защото ги добавяш ръчно
+        }
+    }
+}
