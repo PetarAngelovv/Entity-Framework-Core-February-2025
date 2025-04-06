76267aaf8f8ccf129e926eac98b31556683eaa7b
Petar_Angelov <101021042+PetarAngelovv@users.noreply.github.com>
2025-04-06 14:33:15 +0300

    Add files via upload

---
+++ Extensible Markup Language - XML/01. Import Users/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿namespace ProductShop.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = null!;
+
+  
+        public decimal Price { get; set; }
+
+        [Required]
+        public int SellerId { get; set; }
+        public virtual User Seller { get; set; } = null!;
+
+ 
+        public int? BuyerId { get; set; }
+        public virtual User? Buyer { get; set; }
+
+        public virtual ICollection<CategoryProduct> CategoryProducts { get; set; } = new HashSet<CategoryProduct>();
+    }
+}
---
+++ Extensible Markup Language - XML/01. Import Users/StartUp.cs
@@ -0,0 +1,76 @@
+﻿using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.DTOs.Import;
+using ProductShop.Models;
+using ProductShop.Utilities;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ProductShopContext dbContext = new ProductShopContext();
+
+            string xmlPath = "../../../Datasets/users.xml";
+
+            string XMLInput = File.ReadAllText(xmlPath);
+
+            Console.WriteLine(ImportUsers(dbContext, XMLInput));
+        }
+        public static string ImportUsers(ProductShopContext context, string inputXml)
+        {
+            string result = string.Empty;
+
+            ImportUserDto[] userDtos = XmlHelper.Deserialize<ImportUserDto[]>(inputXml,"Users");
+
+            if(userDtos != null)
+            {
+                ICollection<User> usersToAdd = new HashSet<User>();
+
+                foreach (var dto in userDtos)
+                {
+                    if (!IsValid(dto))
+                    {
+                        continue;
+                    }
+                   int? userAge = null;
+     
+                   if (dto.Age != null)
+                   {
+     
+                       bool isAgeValid = int.TryParse(dto.Age, out int parsedAge);
+                       if (!isAgeValid)
+                       {
+                           continue;
+                       }
+                       userAge = parsedAge;
+                   }
+                    User user = new User()
+                    {
+                        FirstName = dto.FirstName,
+                        LastName = dto.LastName,
+                        Age = userAge
+                    };
+                    usersToAdd.Add(user);
+                }
+                context.Users.AddRange(usersToAdd);
+                //context.SaveChanges();
+                result = $"Successfully imported {usersToAdd.Count}";
+            }
+
+            return result;
+        }
+
+        public static bool IsValid(object dto)
+        {
+            var validateContext = new ValidationContext(dto);
+            var validateResults = new List<ValidationResult>();
+
+            bool isValid = Validator.TryValidateObject(dto, validateContext, validateResults, true);
+
+            return isValid;
+        }
+    }
+}
