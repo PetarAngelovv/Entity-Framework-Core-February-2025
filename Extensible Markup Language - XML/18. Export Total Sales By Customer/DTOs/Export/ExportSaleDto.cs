0d913172849cae3a67defddefe9f2740a0ede4a0
Petar_Angelov <101021042+PetarAngelovv@users.noreply.github.com>
2025-04-07 22:35:58 +0300

    Add files via upload

---
+++ Extensible Markup Language - XML/18. Export Total Sales By Customer/DTOs/Export/ExportSaleDto.cs
@@ -0,0 +1,34 @@
+﻿using CarDealer.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace CarDealer.DTOs.Export
+{
+    [XmlType("Sale")]
+    public class ExportSaleDiscountDto
+    {
+        [XmlElement(nameof(car))]
+        public ExportCarDto[] car { get; set; }
+
+
+        [XmlElement(nameof(discount))]
+        public int discount { get; set; }
+
+        [XmlElement("customer-name")]
+        public string customerName { get; set; }
+
+        [XmlElement(nameof(price))]
+        public decimal price { get; set; }
+
+        [XmlElement("price-with-discount")]
+        public decimal priceWithDiscount { get; set; }
+       
+
+    }
+}
---
+++ Extensible Markup Language - XML/18. Export Total Sales By Customer/DTOs/Import/ImportCustomerDto.cs
@@ -0,0 +1,28 @@
+﻿using CarDealer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace CarDealer.DTOs.Import
+{
+    [XmlType("Customer")]
+    public class ImportCustomerDto
+    {
+        [XmlElement("name")]
+        public string Name { get; set; }
+
+        [XmlElement("birthDate")]
+        public string birthDate { get; set; }
+
+        [XmlElement("isYoungDriver")]
+        public string isYoungDriver { get; set; }
+
+
+
+    }
+}
+
