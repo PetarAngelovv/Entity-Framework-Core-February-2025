374de1124b83e17f93e33a4c47434de6db20e233
Petar_Angelov <101021042+PetarAngelovv@users.noreply.github.com>
2025-04-07 22:34:11 +0300

    Add files via upload

---
+++ Extensible Markup Language - XML/09. Import Suppliers/DTOs/Import/ImportSupplierDto.cs
@@ -0,0 +1,22 @@
+﻿using CarDealer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace CarDealer.DTOs.Import
+{
+    [XmlType("Supplier")]
+    public class ImportSupplierDto
+    {
+        [XmlElement("name")]
+        public string Name { get; set; }
+
+
+        [XmlElement(nameof(isImporter))]
+        public /*string */ bool isImporter { get; set; }
+    }
+}
---
+++ Extensible Markup Language - XML/09. Import Suppliers/StartUp.cs
@@ -0,0 +1,65 @@
+﻿using CarDealer.Data;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+using CarDealer.Utilities;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext dbContext = new CarDealerContext();
+            string inputXml = File.ReadAllText("../../../Datasets/suppliers.xml");
+
+            Console.WriteLine(ImportSuppliers(dbContext, inputXml));
+        }
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            string result = string.Empty;
+            ImportSupplierDto[] importSupDtos = XmlHelper.Deserialize<ImportSupplierDto[]>(inputXml,"Suppliers");
+
+            if (importSupDtos != null)
+            {
+                ICollection<Supplier> suppliersToAdd = new HashSet<Supplier>();
+
+                foreach (var dto in importSupDtos)
+                {
+                    if (!IsValid(dto))
+                    {
+                        continue;
+                    }
+                    Supplier supplier = new Supplier()
+                    {
+                        Name = dto.Name,
+                        IsImporter = dto.isImporter/*ParsedImporter*/
+                    };
+
+                    suppliersToAdd.Add(supplier);
+                }
+                context.AddRange(suppliersToAdd);
+                context.SaveChanges();
+                result = $"Successfully imported {suppliersToAdd.Count}";
+            }
+
+
+           
+            return result;
+        }
+
+        //public static string ImportParts(CarDealerContext context, string inputXml)
+        //{
+
+        //}
+        public static bool IsValid(object dto)
+        {
+            var validateContext = new ValidationContext(dto);
+            var validateResults = new List<ValidationResult>();
+
+            bool isValid = Validator.TryValidateObject(dto, validateContext, validateResults, true);
+
+            return isValid;
+        }
+    }
+}
