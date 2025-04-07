1b3026aa6fe34bf51f5d25f6a4c741fc558a80aa
Petar_Angelov <101021042+PetarAngelovv@users.noreply.github.com>
2025-04-07 22:35:25 +0300

    Add files via upload

---
+++ Extensible Markup Language - XML/16. Export Local Suppliers/DTOs/Export/ExportCarAttributesDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace CarDealer.DTOs.Export
+{
+    [XmlType("car")]
+    public class ExportCarAttributesDto
+    {
+        [XmlAttribute(nameof(id))]
+        public string id { get; set; }
+
+        [XmlAttribute(nameof(model))]
+        public string model { get; set; }
+
+        [XmlAttribute("traveled-distance")]
+        public string traveledDistance { get; set; }
+    }
+}
